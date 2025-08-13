630148e4c2ac6d98c8f19c1eeb8f9eb941cfab6f
crevasse02 <wiboworaffi82@gmail.com>
2025-08-13 13:35:19 +0700

    update socmed

--- resources/views/components/instagram.blade.php
+++ resources/views/components/instagram.blade.php
@@ -1,19 +1,19 @@
 <div class="flex gap-5 justify-center flex-wrap p-4">
-    <a href="https://www.instagram.com/p/DMPriCVxKbk/"
+    <a href="https://www.instagram.com/p/DMz8C_ByADe/"
         target="_blank">
         <img class="h-[450px] " src="{{ asset('assets/img/placeholder/ig_1.webp') }}" alt="Watch on TikTok">
     </a>
 
-    <a href="https://www.instagram.com/p/DMcegm_RU5W/"
+    <a href="https://www.instagram.com/p/DMPriCVxKbk/"
         target="_blank">
         <img class="h-[450px] " src="{{ asset('assets/img/placeholder/ig_2.webp') }}" alt="Watch on TikTok">
     </a>
 
-    <a href="https://www.tiktok.com/@generasiraw/video/7528679067194019077"
+    <a href="https://www.tiktok.com/@generasiraw/video/7533448742515510584"
         target="_blank">
         <img class="h-[450px]" src="{{ asset('assets/img/placeholder/tiktok_1.webp') }}" alt="Watch on TikTok">
     </a>
-    <a href="https://www.tiktok.com/@generasiraw/video/7528775432494222598"
+    <a href="https://www.tiktok.com/@generasiraw/video/7535827237740875013"
         target="_blank">
         <img class="h-[450px]" src="{{ asset('assets/img/placeholder/tiktok_2.webp') }}" alt="Watch on TikTok">
     </a>
