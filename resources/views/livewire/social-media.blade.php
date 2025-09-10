a405f8a58ec8632abc5c4725fbed59d1c8743f35
crevasse02 <wiboworaffi82@gmail.com>
2025-09-10 15:32:44 +0700

    updare

--- resources/views/livewire/social-media.blade.php
+++ resources/views/livewire/social-media.blade.php
@@ -1,19 +1,21 @@
-<div class="flex gap-5 justify-center flex-wrap p-4">
-   
-    @if (!$socmedTiktok->isEmpty())
-        @foreach ($socmedTiktok as $tiktok)
-            <a href="{{ $tiktok->socmed_link }}" target="_blank">
-                <img class="h-[450px]" src="{{ asset('storage/' . $tiktok->thumbnail) }}" alt="Watch on TikTok">
-            </a>
-        @endforeach
-    @endif
-    @if (!$socmedInsta->isEmpty())
-        @foreach ($socmedInsta as $insta)
-            <a href="{{ $insta->socmed_link }}" target="_blank">
-                <img class="h-[450px] " src="{{ asset('storage/' . $insta->thumbnail) }}" alt="Watch on TikTok">
-            </a>
-        @endforeach
-    @endif
+<div>
+
+    <div class="flex gap-5 justify-center flex-wrap p-4">
+        @if (!$socmedTiktok->isEmpty())
+            @foreach ($socmedTiktok as $tiktok)
+                <a href="{{ $tiktok->socmed_link }}" target="_blank">
+                    <img class="h-[450px]" src="{{ asset('storage/' . $tiktok->thumbnail) }}" alt="Watch on TikTok">
+                </a>
+            @endforeach
+        @endif
+        @if (!$socmedInsta->isEmpty())
+            @foreach ($socmedInsta as $insta)
+                <a href="{{ $insta->socmed_link }}" target="_blank">
+                    <img class="h-[450px] " src="{{ asset('storage/' . $insta->thumbnail) }}" alt="Watch on TikTok">
+                </a>
+            @endforeach
+        @endif
+    </div>
 </div>
 <style>
     .instagram-media {
