a724127f097c651b727e20403cc0f254245e2a28
crevasse02 <wiboworaffi82@gmail.com>
2025-11-10 16:19:39 +0700

    stas

--- routes/web.php
+++ routes/web.php
@@ -8,6 +8,8 @@ use App\Http\Controllers\ArticleController;
 use App\Http\Controllers\QuizController;
 use App\Http\Controllers\ExploreController;
 use App\Http\Controllers\GalleryController;
+use App\Http\Controllers\OAuthController;
+use App\Http\Controllers\RawLeagueController;
 use App\Http\Controllers\RawVideosController;
 use App\Http\Controllers\ScheduleController;
 use App\Http\Controllers\WarkopController;
@@ -18,9 +20,6 @@ Route::get('/', function () {
 });
 
 
-Route::get('/raw-league', function () {
-    return view('comingsoon');
-});
 Route::get('/raw-fest', function () {
     return view('comingsoon');
 });
@@ -33,7 +32,6 @@ Route::get('/parents', function () {
 });
 
 // warkop
-
 Route::prefix('warkop-raw')->group(function () {
     // Add your filter route here under the prefix
     Route::get('/', [WarkopController::class, 'index'])->name('warkop');
@@ -47,6 +45,11 @@ Route::prefix('warkop-raw')->group(function () {
 // warkop (function filtering)
     Route::get('/schedules/filter', [ScheduleController::class, 'filter'])->name('schedules.filter');
 
+// rawleague
+Route::prefix('raw-league')->group(function () {
+    Route::get('/', [RawLeagueController::class, 'index'])->name('rawleague');
+
+});
 
 
 // explore
@@ -55,3 +58,20 @@ Route::get('/explore/raw-videos', [RawVideosController::class, 'index'])->name('
 Route::get('/explore/quiz', [QuizController::class, 'index'])->name('explore.quiz.index');
 Route::get('/explore/articles', [ArticleController::class, 'index'])->name('explore.articles.index');
 Route::get('/explore/articles/{slug}', [ArticleController::class, 'show'])->name('explore.articles.show');
+
+
+Route::get('/callback', [OAuthController::class, 'handleCallback']);
+
+// Route::get('/get-token', function () {
+//     // Retrieve the access token and refresh token from the session
+//     $contactForm = new ContactForm();
+//     $accessToken = $contactForm->generateAccessToken();
+//     $refreshToken = config('services.zoho.refresh_token');
+
+
+//     // Return the tokens in a structured response
+//     return response()->json([
+//         'access_token' => $accessToken,
+//         'refresh_token' => $refreshToken,
+//     ]);
+// });
