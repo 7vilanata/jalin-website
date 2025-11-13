43eb8e2be8095864d44006fc05c6a258dd2d3e93
Raffi <wiboworaffi82@gmail.com>
2025-11-13 16:08:48 +0700

    update design

--- resources/views/warkop/gallery/index.blade.php
+++ resources/views/warkop/gallery/index.blade.php
@@ -49,21 +49,21 @@
                                                 <h4 class=" mt-4 text-[#0353FF] text-[18px] text-left font-bold ">
                                                     {{ $gallery->title }}
                                                 </h4>
-                                                <div class="text-left flex gap-4 md:gap-10 text-[10px] md:text-[14px]">
+                                                <div class="text-center flex gap-4 md:gap-10 text-[10px] md:text-[14px]">
                                                     <span class="flex items-center gap-1"><svg
                                                             xmlns="http://www.w3.org/2000/svg" fill="none"
                                                             viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
-                                                            class="size-3 md:size-4">
+                                                            class="size-3 md:size-4 min-w-3">
                                                             <path stroke-linecap="round" stroke-linejoin="round"
                                                                 d="M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
                                                             <path stroke-linecap="round" stroke-linejoin="round"
                                                                 d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1 1 15 0Z" />
                                                         </svg>
-                                                        {{ $gallery->street_loc }}</span>
+                                                        {{ Str::limit( $gallery->street_loc, 50, '...') }}</span>
                                                     <span class="flex items-center gap-1"><svg
                                                             xmlns="http://www.w3.org/2000/svg" fill="none"
                                                             viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
-                                                            class="size-3 md:size-4">
+                                                            class="size-3 md:size-4 min-w-3">
                                                             <path stroke-linecap="round" stroke-linejoin="round"
                                                                 d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 0 1 2.25-2.25h13.5A2.25 2.25 0 0 1 21 7.5v11.25m-18 0A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75m-18 0v-7.5A2.25 2.25 0 0 1 5.25 9h13.5A2.25 2.25 0 0 1 21 11.25v7.5" />
                                                         </svg>
